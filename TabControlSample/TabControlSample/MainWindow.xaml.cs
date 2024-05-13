85ff9a132eeb28deefb4bbf4cb5546ed2604bca0
Nastya <105735728+NastyaGalinovskaya@users.noreply.github.com>
2024-05-13 17:19:23 +0300

    TabControlSample
    TabControlSample

---
+++ TabControlSample/TabControlSample/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TabControlSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
