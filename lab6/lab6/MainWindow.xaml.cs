a4838af8c811928a6d3c0d89685a1291e11108a8
Nastya <105735728+NastyaGalinovskaya@users.noreply.github.com>
2024-05-23 12:33:19 +0300

    lab6
    DataGrid

---
+++ lab6/lab6/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace lab6
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<User> users = new List<User>();
+            users.Add(new User() { Id = 1, Name = "Rick Sanchez", Birthday = new DateTime(1971, 7, 23), ImageUrl = "D:\\учеба\\3 курс 2 семестр\\візуал програмування\\lab6\\Rick_Sanchez.jpg" });
+            users.Add(new User() { Id = 2, Name = "Doctor Who", Birthday = new DateTime(1994, 1, 17), ImageUrl = "D:\\учеба\\3 курс 2 семестр\\візуал програмування\\lab6\\doctor.jpg" });
+            users.Add(new User() { Id = 3, Name = "Barbie Girl", Birthday = new DateTime(2002, 9, 2), ImageUrl = "D:\\учеба\\3 курс 2 семестр\\візуал програмування\\lab6\\barbie.jpg" });
+            dgUsers.ItemsSource = users;
+        }
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public DateTime Birthday { get; set; }
+            public string ImageUrl { get; set; }
+        }
+    }
+}
